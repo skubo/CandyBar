debf8ff633c24bf99160627d1683183b8778f7e0
txitax <txitax@Abraxass-MacBook-Pro-2.local>
2015-07-06 16:16:36 +0200

    adding background PDF, adding custom font, adding constructors and text drawing

--- CandyBar/CandyBarView.h
+++ CandyBar/CandyBarView.h
@@ -2,12 +2,18 @@
 //  CandyBarView.h
 //  CandyBar
 //
-//  Created by txitax on 02.07.15.
+//  Created by skubo on 02.07.15.
 //  Copyright (c) 2015 ga.rbers.net. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface CandyBarView : UIView
+
+-(id)initWithFrame:(CGRect)frame andTitle:(NSString*) title;
+
+-(void)setValue:(float)value lowValue:(float) low highValue:(float) high;
+
+@property (nonatomic, strong) UILabel* barTitle;
 
 @end
--- CandyBar/CandyBarView.m
+++ CandyBar/CandyBarView.m
@@ -2,13 +2,85 @@
 //  CandyBarView.m
 //  CandyBar
 //
-//  Created by txitax on 02.07.15.
+//  Created by skubo on 02.07.15.
 //  Copyright (c) 2015 ga.rbers.net. All rights reserved.
 //
 
 #import "CandyBarView.h"
+#import <CoreText/CoreText.h>
 
 @implementation CandyBarView
+
++ (void)load
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        // Dynamically load bundled custom fonts
+        
+        [self loadFont];
+    });
+}
+
++ (void)loadFont
+{
+    NSBundle* bundle = [NSBundle bundleForClass:[CandyBarView class]];
+    
+    NSString *fontPath = [bundle pathForResource:@"SNICN___" ofType:@"TTF"];
+    NSData *fontData = [NSData dataWithContentsOfFile:fontPath];
+    
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((CFDataRef)fontData);
+    
+    if (provider)
+    {
+        CGFontRef font = CGFontCreateWithDataProvider(provider);
+        
+        if (font)
+        {
+            CFErrorRef error = NULL;
+            if (CTFontManagerRegisterGraphicsFont(font, &error) == NO)
+            {
+                CFStringRef errorDescription = CFErrorCopyDescription(error);
+                NSLog(@"Failed to load font: %@", errorDescription);
+                CFRelease(errorDescription);
+            }
+            
+            CFRelease(font);
+        }
+        
+        CFRelease(provider);
+    }
+}
+
+-(id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        UIImageView* barImageView = [[UIImageView alloc] initWithFrame:self.bounds];
+        barImageView.image = [UIImage imageNamed:@"frame" inBundle:[NSBundle bundleForClass:self.class] compatibleWithTraitCollection:nil];
+        [self addSubview:barImageView];
+        self.backgroundColor = [UIColor clearColor];
+        
+        _barTitle = [[UILabel alloc] initWithFrame:self.bounds];
+        _barTitle.textColor = [UIColor blackColor];
+        _barTitle.text = @"CandyMan";
+        _barTitle.textAlignment = NSTextAlignmentCenter;
+        
+        _barTitle.font = [UIFont fontWithName:@"SnickersNormal" size:[self calculateFontSize]];
+        [self addSubview:_barTitle];
+    }
+    
+    return self;
+}
+
+-(id)initWithFrame:(CGRect)frame andTitle:(NSString*) title {
+    if ([self initWithFrame:frame]) {
+        _barTitle.text = title;
+    }
+    return self;
+}
+
+-(float)calculateFontSize {
+    // TODO
+    return 50.0f;
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
@@ -18,4 +90,7 @@
 }
 */
 
+-(void)setValue:(float)value lowValue:(float) low highValue:(float) high {
+    
+}
 @end
